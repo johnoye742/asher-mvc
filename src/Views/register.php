44989380673187684ef493a57d8c37d14c18f4f4
John Oye <olugbengajohnoye@gmail.com>
2024-03-16 13:13:42 +0100

    No major change yet

--- src/Views/register.php
+++ src/Views/register.php
@@ -8,7 +8,15 @@
 <body class="w-full h-[100dvh] flex flex-col items-center justify-center">
     <div class="lg:w-[50%] h-fit">
         <h1 class="uppercase text-2xl">Register</h1>
-        <input type="text" placeholder="Enter preffered username" name="username" class="p-2 bg-gray-100">
+        <input type="text" placeholder="Enter preffered username" name="username" class="p-2 bg-gray-100"><br/><br/>
+        <label for='role'>
+            Role:
+        </label>
+        <select id="role" name="role">
+            <option>User</option>
+            <option>Admin</option>
+        </select><br/><br/>
+        <input type="text" placeholder="Enter preffered password" name="pwd" class="p-2 bg-gray-100">
     </div>
 </body>
 </html>
